dc62ca427f8f0c6f10b6d7b1fbdf5855305b615f
Sebastian Lindgren <sebastian.lindgrenen@gmail.com>
2023-12-05 14:46:08 +0100

    fixed mouth

--- mouthDetection.m
+++ mouthDetection.m
@@ -1,13 +1,14 @@
 function mouthMap = mouthDetection(YCrCb)
 
-    Cb = normalize(double(YCrCb(:,:,2)), 'norm', 1);
-    Cr = normalize(double(YCrCb(:,:,3)), 'norm', 1);
+    Cb = double(YCrCb(:,:,2));
+    Cr = double(YCrCb(:,:,3));
 
     % Use the paper's formula for eta
     eta = 0.95 * (0.5 * sum(Cr.^2)) / (0.5 * sum(Cr./Cb));
-
     % Normalize the mouth map between 0 and 1
     mouthMapFormula = Cr.^2 .* (Cr.^2 - eta * Cr./Cb).^2;
+
+    mouthMapFormula = (mouthMapFormula - min(mouthMapFormula(:))) / (max(mouthMapFormula(:)) - min(mouthMapFormula(:)));
     threshold = 0.5;
     mouthMap = mouthMapFormula > threshold;
 
